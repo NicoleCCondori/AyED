f8dd528b02acf7a52d214d59a6e8b425ac27fef3
Nicole Lujan Condori Cordova <ncondoricordova@frba.utn.edu.ar>
2023-09-28 20:38:59 -0300

    corrección

---
+++ 01-CalcularPi/README.md
@@ -0,0 +1,13 @@
+Calcular pi
+Consigna:
+/*a. Se desea calcular el valor de π usando la serie de Liebniz, 
+sin usar ninguna optimización.
+b. Debe iterar hasta que el valor de π se correcto en sus 6 primeros 
+decimales (3,141592)*/
+
+Use long double para una mayor precisión.
+Según la fórmula de Leibniz , si es par o impar se suma o se resta.
+Me guíe de la fórmula pi= 4*(1-1/3+1/5-1/7+1/9-...)
+El 1e-6 es la precisión de 6 decimales (0,000001)
+
+El valor de pi: 3,141592
--- 01-CalcularPi/main.cpp
+++ 01-CalcularPi/main.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
-#include <fstream>
 #include <iomanip>
-#include <math.h> //potencia
-
-/*a. Se desea calcular el valor de π usando la serie de Liebniz, sin usar ninguna optimización.
-b. Debe iterar hasta que el valor de π se correcto en sus 6 primeros decimales (3,141592)*/
-
+#include <cmath>
+/*a. Se desea calcular el valor de π usando la serie de Liebniz, 
+sin usar ninguna optimización.
+b. Debe iterar hasta que el valor de π se correcto en sus 6 primeros 
+decimales (3,141592)*/
 using namespace std;
-int main()
+
+int main() 
 {
-    int i;
-    float pi = 0;
-    for(i=0; i<=1000000 ; i++)
-        pi += (pow(-1,i))/(2*i+1);
-    cout << "El valor de pi: " << fixed << setprecision(6) << pi*4 << endl;
+    long double pi = 0;
+    long double precision = 1;
+    int i = 0;
+
+    do {
+        if (i % 2 == 0) 
+        {
+            pi += 4 / precision;
+        } else {
+            pi -= 4 / precision;
+        }
+        precision += 2;
+        i++;
+    } while (abs(M_PI - pi) >= 1e-6); 
+
+    cout << "El valor de pi: " << fixed << setprecision(6) << pi << endl;
     return 0;
 }
